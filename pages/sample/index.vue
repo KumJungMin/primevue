5cef207d4258b49cb9be55df0ff0bf7050cedd71
KumJungMin <rmawjdals@naver.com>
2024-05-18 19:58:55 +0900

    add resolve issue generation code

---
+++ pages/sample/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- TODO: test this -->
+    <div>
+        <div :style="{ backgroundColor: 'red', width: '200vw', height: '20px' }"></div>
+        <Dialog v-model:visible="visible" modal header="My modal" :style="{ width: '50vw' }">
+            <Dropdown v-model="selected" :options="items" placeholder="Select an item" />
+        </Dialog>
+        <Button label="Open Modal" @click="visible = true" />
+    </div>
+</template>
+
+<script>
+import Dropdown from 'primevue/dropdown';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+
+export default {
+    setup() {
+        definePageMeta({
+            layout: ''
+        });
+    },
+    data() {
+        return {
+            visible: false,
+            selected: null,
+            items: [
+                { label: 'Item 1', value: 'Item 1' },
+                { label: 'Item 2', value: 'Item 2' },
+                { label: 'Item 3', value: 'Item 3' }
+            ]
+        };
+    },
+    components: {
+        Dropdown,
+        Dialog,
+        Button
+    }
+};
+</script>
